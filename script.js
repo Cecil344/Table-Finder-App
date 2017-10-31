d9f00cef7bf62b7756638ea4da56956558b7bf37
Cecil Johnson <cecil344@gmail.com>
2017-10-31 09:00:12 -0400

    lab

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+console.log("Hi");
+
+$("#button1").click(function(){
+  $(".hiddenForm").show();
+});
+
+$("#xButton").click(function(){
+  $(".hiddenForm").hide();
+});
+
+
+
+
+
+
+
+
+
+
+//end of jQuery thing that needs to be at the bottom
+})
